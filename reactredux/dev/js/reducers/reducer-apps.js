6a9d04c71069ba99faa52e3c16313a3212361a50
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-11 19:59:28 -0400

    Update reactredux/dev/js/reducers/reducer-apps.js

--- reactredux/dev/js/reducers/reducer-apps.js
+++ reactredux/dev/js/reducers/reducer-apps.js
@@ -7,7 +7,6 @@ export default function() {
 			id: 1,
 			first: "testone",
 			last: "testone",
-			age: 33,
 			description: "Just put some stuff here",
 			thumbnail: "http://i.imgur.com/7yUvePI.jpg"
 		},
@@ -15,7 +14,6 @@ export default function() {
 			id: 2,
 			first: "testtwo",
 			last: "testtwo",
-			age: 53,
 			description: "Other stuff here",
 			thumbnail: ""
 		},
@@ -23,7 +21,6 @@ export default function() {
 			id: 3,
 			first: "testthree",
 			last: "testthree",
-			age: 55,
 			description: "Stuff",
 			thumbnail: ""
 		}
