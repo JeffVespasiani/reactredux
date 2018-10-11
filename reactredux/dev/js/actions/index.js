537c673c6647b9abc61b025449d8d1fc7e66fa02
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-11 19:49:00 -0400

    Update reactredux/dev/js/actions/index.js

--- reactredux/dev/js/actions/index.js
+++ reactredux/dev/js/actions/index.js
@@ -1,6 +1,23 @@
+import axios from "axios";
+
 export const selectApp = (app) => {
 	return {
 		type: "APP_SELECTED",
 		appname: app
 	}
 };
+
+export function loadJson(){
+	return(dispatch)=>{
+		return axios.get("http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=100/json").then((response)=>{
+			dispatch(getInfo(response.data.feed.entry));
+		})
+	}
+}
+
+export function getInfo(theInfo){
+	return{
+		type: "JSON_DATA",
+		info: theInfo
+	}
+}
