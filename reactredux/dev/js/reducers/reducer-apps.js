2a53d93a0c107f60f030b069622e0fd107f99a4d
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-11 18:38:09 -0400

    Update reactredux/dev/js/reducers/reducer-apps.js

--- reactredux/dev/js/reducers/reducer-apps.js
+++ reactredux/dev/js/reducers/reducer-apps.js
@@ -1,8 +1,35 @@
 
 export default function() {
-		
-	return fetch('http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=100/json')
-		.then((Response) => Response.json());
+	return [
+		{
+			id: 1,
+			first: "Bucky",
+			last: "Roberts",
+			age: 71,
+			description: "Just put some stuff here",
+			thumbnail: "http://i.imgur.com/7yUvePI.jpg"
+		},
+		{
+			id: 2,
+			first: "Joby",
+			last: "Wasilenko",
+			age: 27,
+			description: "Other stuff here",
+			thumbnail: ""
+		},
+		{
+			id: 3,
+			first: "Madison",
+			last: "Williams",
+			age: 24,
+			description: "Stuff",
+			thumbnail: ""
+		}
+	]
+	
+	
+	/*return fetch('http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=100/json')
+		.then((Response) => Response.json());*/
 		
 		
 	
