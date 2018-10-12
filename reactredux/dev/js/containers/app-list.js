86959177e53265f993326a85baeec801faaee84b
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-12 16:17:58 -0400

    Update reactredux/dev/js/containers/app-list.js

--- reactredux/dev/js/containers/app-list.js
+++ reactredux/dev/js/containers/app-list.js
@@ -6,7 +6,7 @@ import {selectApp} from '../actions/index';
 class AppList extends Component {
 	
 	createAppList(){
-		console.log(this.props.apps)
+	
 	return this.props.apps.map((app) => {
 			return (
 				<li><button key={app.id} className="menu"  onClick={() => this.props.selectApp(app)}>{app.first}</button></li>
@@ -14,6 +14,7 @@ class AppList extends Component {
 		});
 	}
 	
+	//The this.createAppList() in the render uses curly brackets to exit the JSX statement to call in the createAppList function to render the list of apps.
 	render() {
 		return (
 			<ul>
@@ -24,14 +25,17 @@ class AppList extends Component {
 	
 }
 
+//This sets the apps attribute of the state to the Apps API retrieved from the reducers, and passes it to the function as a prop.  This also automatically updates whenever a that state is changed.
 function mapStateToProps(state) {
 	return {
 		apps: state.apps
 	}
 }
 
+//This gets the actions and gives them to the app as props.  It allows the app to use the selectApp function in Actions.
 function mapDispatchToProps(dispatch){
 	return bindActionCreators({selectApp: selectApp}, dispatch);
 }
 
+//This exports the function of the program so it can be accessed by the rest of the program.  It passes the mapStateToProps and mapDispatchToProps functions as attributes.
 export default connect(mapStateToProps, mapDispatchToProps)(AppList);
