a8526c8117dc66ed6513c2e46b071b24c13058e1
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-12 16:17:50 -0400

    Update reactredux/dev/js/containers/app-detail.js

--- reactredux/dev/js/containers/app-detail.js
+++ reactredux/dev/js/containers/app-detail.js
@@ -5,8 +5,9 @@ class AppDetail extends Component {
 	
     render() {
 		
+		//If the user hasn't picked anything yet, this default value is shown.
         if (!this.props.app) {
-            return (<div>Select a user...</div>);
+            return (<div>Select an app...</div>);
         }
         return (
             <div>
@@ -17,6 +18,7 @@ class AppDetail extends Component {
     }
 }
 
+//This brings in the function in Actions that declares which app was picked by the user.
 function mapStateToProps(state) {
     return {
         app: state.active
