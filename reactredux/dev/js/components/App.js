2392a9e39ba426a4f2182e715adc4640f320c4d0
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-12 16:20:44 -0400

    Update reactredux/dev/js/components/App.js

--- reactredux/dev/js/components/App.js
+++ reactredux/dev/js/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppList from '../containers/app-list';
 import AppDetail from '../containers/app-detail'
 
+//This calls in the functions from the containers that show the information requested
 const App = () => (
 	<div>
 		<h2>App Name List</h2>
