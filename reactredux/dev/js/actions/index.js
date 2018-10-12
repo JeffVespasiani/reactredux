b61c5db8b835656102f1870dfdd4fee0bb267523
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-12 14:26:44 -0400

    Update reactredux/dev/js/actions/index.js

--- reactredux/dev/js/actions/index.js
+++ reactredux/dev/js/actions/index.js
@@ -7,17 +7,49 @@ export const selectApp = (app) => {
 	}
 };
 
+export function somethingsWrong(){
+	return{
+		type: 'JSON_ERROR',
+	};
+}
+
+export function loadingJson(){
+	return{
+		type: 'JSON_LOADING',
+	};
+}
+
+export function jsonSuccess(appInfo){
+	return{
+		type: 'JSON_FOUND',
+		appInfo
+	};
+}
+/*
+export function fetchJson(){
+	return (dispatch) => {
+		dispatch(loadingJson());
+		fetch('http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=100/json')
+		.then((response)=>{
+			return response;
+		})
+		.then((response) => response.json())
+		.then((appItems) => dispatch(jsonSuccess(appItems))
+		.catch(() => dispatch(somethingsWrong());
+	};
+}
+*/
 export function loadJson(){
 	return(dispatch)=>{
 		return axios.get("http://phobos.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=100/json").then((response)=>{
 			dispatch(getInfo(response.data.feed.entry));
 		})
 	}
-}
+};
 
 export function getInfo(theInfo){
 	return{
 		type: "JSON_DATA",
 		apps: theinfo
 	}
-}
+};
