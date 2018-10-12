0dc2aa41371a1591eab7692319e88b259b145dc9
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-11 21:25:12 -0400

    Update reactredux/dev/js/reducers/reducer-apps.js

--- reactredux/dev/js/reducers/reducer-apps.js
+++ reactredux/dev/js/reducers/reducer-apps.js
@@ -7,21 +7,18 @@ export default function() {
 			first: "testone",
 			last: "testone",
 			description: "Just put some stuff here",
-			thumbnail: "http://i.imgur.com/7yUvePI.jpg"
 		},
 		{
 			id: 2,
 			first: "testtwo",
 			last: "testtwo",
 			description: "Other stuff here",
-			thumbnail: ""
 		},
 		{
 			id: 3,
 			first: "testthree",
 			last: "testthree",
 			description: "Stuff",
-			thumbnail: ""
 		}
 	]
 	
