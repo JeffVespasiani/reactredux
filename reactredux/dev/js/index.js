a520cddae9b28e48761f386071ef212468e2d18a
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-12 15:07:33 -0400

    Update reactredux/dev/js/index.js

--- reactredux/dev/js/index.js
+++ reactredux/dev/js/index.js
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import allReducers from './reducers';
 import App from './components/App';
+import '../scss/style.scss';
 
 const store = createStore(allReducers);
 
