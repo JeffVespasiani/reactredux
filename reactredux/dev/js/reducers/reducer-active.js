8f8171c078bfef8ef13cec99358b85b34f98cb55
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-12 16:25:03 -0400

    Update reactredux/dev/js/reducers/reducer-active.js

--- reactredux/dev/js/reducers/reducer-active.js
+++ reactredux/dev/js/reducers/reducer-active.js
@@ -1,3 +1,5 @@
+//This reducer's responsible for setting the state when the user picks the app.
+//The state is set to a default value of "null" to prevent errors upon starting up the program
 export default function(state=null, action) {
 	switch(action.type) {
 		case "APP_SELECTED":
