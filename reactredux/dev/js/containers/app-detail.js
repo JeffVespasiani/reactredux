91694c8a523d93f58b7cbe3c4d7880ff40b4ff82
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-12 20:35:22 -0400

    Update reactredux/dev/js/containers/app-detail.js

--- reactredux/dev/js/containers/app-detail.js
+++ reactredux/dev/js/containers/app-detail.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import moment from 'moment';
 
 class AppDetail extends Component {
 	
@@ -11,8 +12,14 @@ class AppDetail extends Component {
         }
         return (
             <div>
+				<img src={this.props.app['im:image'][2].label} />
 				<h2>{this.props.app['im:name'].label} </h2>
+				<p>Release date: {moment(this.props.app['im:releaseDate'].label).format('MM/DD/YYYY')}</p>
 				<p>{this.props.app.summary.label}</p>
+				<p>Price: {this.props.app['im:price'].label} {this.props.app['im:price'].attributes.currency}</p>
+				<p>Category: {this.props.app.category.attributes.label}</p>
+				<p><a href={this.props.app.link.attributes.href}>Buy it here!</a></p>
+				<p>Publisher: <a href={this.props.app['im:artist'].attributes.href}>{this.props.app['im:artist'].label}</a></p>
 			</div>
         );
     }
