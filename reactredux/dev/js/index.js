4c54fd27eb0e19c93a53d7d6c1e4876c0670ace9
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-14 00:25:46 -0400

    Update reactredux/dev/js/index.js

--- reactredux/dev/js/index.js
+++ reactredux/dev/js/index.js
@@ -2,14 +2,15 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import allReducers from './reducers';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import allReducers from './reducers/index';
 import App from './components/App';
 import '../scss/style.scss';
 
-const store = createStore(allReducers);
+const store = applyMiddleware(thunk)(createStore);
 
 ReactDOM.render(
-	<Provider store={store}>
+	<Provider store={store(allReducers)}>
 		<App />
 	</Provider>, document.getElementById('root'));
