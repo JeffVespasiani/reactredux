5cf9e8fb6003990ca999764ff3ba2ee1c85ea3d9
JeffVespasiani <43939336+JeffVespasiani@users.noreply.github.com>
2018-10-14 00:26:14 -0400

    Update reactredux/dev/js/reducers/index.js

--- reactredux/dev/js/reducers/index.js
+++ reactredux/dev/js/reducers/index.js
@@ -1,10 +1,9 @@
 import {combineReducers} from 'redux';
-import AppReducer from './reducer-apps.js';
+import {appReducer} from './reducer-apps.js';
 import AppChosen from './reducer-active';
 
-//This combines all of the reducers into one object to dispatch the data to the store.
 const allReducers = combineReducers({
-	apps: AppReducer,
+	appStuff: appReducer,
 	active: AppChosen
 });
 
